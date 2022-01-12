c0005a33a23459ddf28508e1ddf113f8cc6e0ac0
Igor Artemov <i_artemov@omobus.net (zlts)>
2022-01-12 15:17:07 +0300

    3.5.5 release

--- version.lua
+++ version.lua
@@ -1,10 +1,10 @@
 -- -*- Lua -*-
--- Copyright (c) 2006 - 2019 omobus-ark authors, see the included COPYRIGHT file.
+-- Copyright (c) 2006 - 2022 omobus-ark authors, see the included COPYRIGHT file.
 
 local M = {} -- public interface
 
 M.package_code		= "ark"
-M.package_version	= "3.5.4"
+M.package_version	= "3.5.5"
 M.package_name		= "omobus-" .. M.package_code
 
 return M
